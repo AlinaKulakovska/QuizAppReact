33d571751fafc55b117b6c839f5c443ff0b1113d
AlinaKulakovska <103748970+AlinaKulakovska@users.noreply.github.com>
2023-05-12 15:32:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,24 +4,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from './Pages/Main.js';
 import Quizes from './Pages/Quizes.js';
 import Results from './Pages/Results.js';
+import { HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-//     <BrowserRouter>
-//       <Routes>
-//       <Route exact path='/' render={<Main />} basename={process.env.PUBLIC_URL}/>
-//         <Route index element={<Main />} />
-//         <Route path="/quizes" element={<Quizes />} />
-//         <Route path="/results" element={<Results />} />
-//       </Routes>
-//     </BrowserRouter>
+    <HashRouter >
+      <Routes>
+      <Route exact path='/' render={<Main />} basename={process.env.PUBLIC_URL}/>
+        <Route index element={<Main />} />
+        <Route path="/quizes" element={<Quizes />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </HashRouter >
 
-<Router basename="/QuizAppReact"> //add basename
-      <Switch>
-        <Route path='/' exact component={Main} />
-        <Route path='/quizes' component={Quizes} />
-      </Switch>
-</Router>
+
   );
 }
 
